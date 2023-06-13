72b9f56efc743e299ff43fa2feaf80a48eef0bbe
Alistair Scott <ali.scott@gmail.com>
2023-06-13 14:15:39 +0100

    chore: test private module

--- infra/dev/us1/ec2/main.tf
+++ infra/dev/us1/ec2/main.tf
@@ -10,3 +10,22 @@ resource "aws_instance" "web_app" {
   ami           = "ami-674cbc1e"
   instance_type = "t3.large"
 }
+
+module "ec2_instance" {
+  source  = "git@github.com:infracost/terraform-private-module-example.git"
+  version = "~> 3.0"
+
+  name = "single-instance"
+
+  ami                    = "ami-ebd02392"
+  instance_type          = "t2.micro"
+  key_name               = "user1"
+  monitoring             = true
+  vpc_security_group_ids = ["sg-12345678"]
+  subnet_id              = "subnet-eddcdzz4"
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
